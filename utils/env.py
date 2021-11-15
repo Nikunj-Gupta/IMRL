7a7071cd682f7f53708bedeee8fb2328eb9938b5
Nikunj Gupta <ng2531@nyu.edu>
2021-11-14 21:42:45 -0500

    added logic to use hammer env wrapper

--- utils/env.py
+++ utils/env.py
@@ -2,7 +2,9 @@ import gym
 import gym_minigrid
 
 
-def make_env(env_key, seed=None):
+def make_env(env_key, seed=None, hammer=False):
     env = gym.make(env_key)
+    if hammer: 
+        env = gym_minigrid.wrappers.HammerObsWrapper(env) # Get hammer observations 
     env.seed(seed)
     return env
