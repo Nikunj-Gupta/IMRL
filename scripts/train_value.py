ba7e3f887e92ccb97b11172831bdda33b57dbb13
Nikunj Gupta <ng2531@nyu.edu>
2022-01-09 19:41:50 -0500

    updated max frames to 100M

--- scripts/train_value.py
+++ scripts/train_value.py
@@ -22,8 +22,8 @@ if __name__ == '__main__':
                         help="number of updates between two saves (default: 10, 0 means no saving)")
     parser.add_argument("--procs", type=int, default=16,
                         help="number of processes (default: 16)")
-    parser.add_argument("--frames", type=int, default=10*10**6,
-                        help="number of frames of training (default: 10M)")
+    parser.add_argument("--frames", type=int, default=100*10**6,
+                        help="number of frames of training (default: 100M)")
 
     ## Parameters for main algorithm
     parser.add_argument("--epochs", type=int, default=4,
